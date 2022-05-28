b6bd7f672504e6d45e1212ddd54058bb8a864927
= <=>
2022-05-27 21:19:14 -0800

    assembly controller recipes added

---
+++ barrel_11.lua
@@ -0,0 +1,14 @@
+local recipes = {}
+
+local mc = "minecraft:"
+local pn = "pneumaticcraft:"
+local rf = "rftools:"
+local kjs = "kubejs:"
+local th = "thermal:"
+
+recipes[pn.."unassembled_pcb"] = {1, pn.."empty_pcb", 1}
+recipes[pn.."advanced_pressure_tube"] = {8, pn.."compressed_iron_block", 1}
+recipes[rf.."machine_frame"] = {1, kjs.."rough_machine_frame", 1}
+recipes[kjs.."rough_machine_frame_top"] = {1, th.."machine_frame", 2}
+
+return recipes
